8eee935f06e50cda0d887ec75a0abb2a18f70f18
Aliaksandr Sekunau <alexandrsekunov@gmail.com>
2025-10-26 19:13:54 +0300

    feat: implement fs:create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import { writeFile } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const CONFIG = {
+  file: 'fresh.txt',
+  dirName: 'files',
+  content: 'I am fresh and young',
+  errorMessage: 'FS operation failed',
+};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const targetDirPath = join(__dirname, CONFIG.dirName);
+const targetFilePath = join(targetDirPath, CONFIG.file);
+
 const create = async () => {
-  // Write your code here
+  try {
+    await writeFile(targetFilePath, CONFIG.content, {
+      encoding: 'utf8',
+      flag: 'wx',
+    });
+  } catch (err) {
+    throw new Error(CONFIG.errorMessage);
+  }
 };
 
 await create();
