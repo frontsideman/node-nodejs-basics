abc453fd552be2859d1bb36fdd46f70bb9ffa112
Aliaksandr Sekunau <alexandrsekunov@gmail.com>
2025-10-26 19:22:30 +0300

    feat: implement fs:list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,23 @@
+import { readdir } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const CONFIG = {
+  dirName: 'files',
+  errorMessage: 'FS operation failed',
+};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const targetDirPath = join(__dirname, CONFIG.dirName);
+
 const list = async () => {
-  // Write your code here
+  try {
+    const files = await readdir(targetDirPath);
+    console.log(files);
+  } catch (err) {
+    throw new Error(CONFIG.errorMessage);
+  }
 };
 
 await list();
