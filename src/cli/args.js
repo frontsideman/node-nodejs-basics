df6584fa0bf01b4b9e47f249f94801452c9adb29
Aliaksandr Sekunau <alexandrsekunov@gmail.com>
2025-10-26 23:16:31 +0300

    feat: implement cli

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-  // Write your code here
+  const resArray = [];
+  const args = process.argv.slice(2);
+
+  for (let i = 0; i < args.length; i++) {
+    if (args[i]?.startsWith('--')) {
+      if (args[i + 1]?.startsWith('--')) {
+        resArray.push(`${args[i]} undefined`);
+        continue;
+      }
+      resArray.push(`${args[i]} ${args[i + 1]}`);
+    }
+  }
+
+  return resArray.join(' ');
 };
 
 parseArgs();
