e16840334887ac5b3e9968ab87f2817206db5d53
Aliaksandr Sekunau <alexandrsekunov@gmail.com>
2025-10-27 01:05:44 +0300

    feat: implement hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,25 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+import { join } from 'node:path';
+
+const CONFIG = {
+  file: 'fileToCalculateHashFor.txt',
+  dirName: 'files',
+};
+
+const __dirname = import.meta.dirname;
+const targetFilePath = join(__dirname, CONFIG.dirName, CONFIG.file);
+
 const calculateHash = async () => {
-  // Write your code here
+  const hash = createHash('sha256');
+
+  const input = createReadStream(targetFilePath);
+  input.pipe(hash);
+  input.on('end', () => {
+    const finalHash = hash.digest('hex');
+    console.log(finalHash);
+  });
 };
 
 await calculateHash();
