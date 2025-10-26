3286737c0e063ec9a68e1f5bb71bab778e50414d
Aliaksandr Sekunau <alexandrsekunov@gmail.com>
2025-10-26 19:31:10 +0300

    feat: implement fs:delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,25 @@
+import { unlink } from 'node:fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const CONFIG = {
+  file: 'fileToRemove.txt',
+  dirName: 'files',
+  errorMessage: 'FS operation failed',
+};
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const targetDirPath = join(__dirname, CONFIG.dirName);
+const targetFilePath = join(targetDirPath, CONFIG.file);
+
 const remove = async () => {
-  // Write your code here
+  try {
+    await unlink(targetFilePath);
+    console.log(`successfully deleted ${CONFIG.file}`);
+  } catch (error) {
+    throw new Error(CONFIG.errorMessage);
+  }
 };
 
 await remove();
